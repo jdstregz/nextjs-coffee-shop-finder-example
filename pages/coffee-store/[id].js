c32ba931169740b7236bb5c8d2ad31241e268e38
Joshua Streger <Joshua.Streger@tsimagine.com>
2022-03-22 18:09:25 -0400

    first commit

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,13 @@
+import {useRouter} from "next/router";
+import Link from "next/link";
+
+const CoffeeStore = () => {
+    const router = useRouter();
+    return (
+        <div>Coffee Store Page {router.query.id}
+            <Link href={"/"}><a>Back to Home</a></Link>
+        </div>
+    )
+}
+
+export default CoffeeStore;
